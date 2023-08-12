21c7b89d12fb7a1c3e8a7906318ed3b6f7354f9f
k <66728641+kingazm@users.noreply.github.com>
2023-08-12 19:32:59 +0200

    setTimeout() added and updated, parameters of the animated ball updated

--- codeBreathee.js
+++ codeBreathee.js
@@ -5,6 +5,8 @@
 //ctx.arc(95,50,40,0,2*Math.PI);
 //ctx.stroke();
 
+
+
 document.getElementById("instruction").innerHTML = "success";
 //changes instruction to next instruction
 //make that dependand on timer (make the timer)
@@ -29,9 +31,17 @@ function draw() {
   //the dot moving mechanics
   const time = new Date();
   ctx.rotate(
-    ((2 * Math.PI) / 60) * time.getSeconds() +
-      ((2 * Math.PI) / 60000) * time.getMilliseconds(),
+    ((2 * Math.PI) / 7) * time.getSeconds() +
+      ((2 * Math.PI) / 7000) * time.getMilliseconds(),
   );
+  //formula adjusted (numbers 4.1, 4100) so that one
+  //circle takes up about seconds (+/-0.5s)
+  //consistent with the box breathing method
+  //update 12.08 7:29 pm: slow down the ball
+  // its meant to be calming mechanics
+  //mix of box breathing and calm breaths!!!
+  //begin with calm small breaths and then etc.
+
   ctx.translate(105, 0);
   ctx.drawImage(dot, -12, -12, 30, 30);
   ctx.restore();
@@ -45,4 +55,16 @@ function draw() {
 
 init();
 
-//set the timer when page loads to track when to show inhale, hold, exhale (optionaly, when hold, stop motion)
+function theFunction(){
+    document.getElementById("instruction").innerHTML = "lalala";
+}
+
+setTimeout(theFunction,4000);
+
+
+//setTimeout(alert('Hello'),3000); checked that timeout works
+//set the timer when page loads to track when to show inhale,
+//hold, exhale (optionaly, when hold, stop motion)
+
+//when this is done, try and optimize the mechanics
+//with switch or sth
