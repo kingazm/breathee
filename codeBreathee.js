0ed4a0730b7e03b2879dbe3529e5eb6ee81c006b
k <66728641+kingazm@users.noreply.github.com>
2023-08-27 21:05:21 +0200

    Update codeBreathee.js

--- codeBreathee.js
+++ codeBreathee.js
@@ -61,28 +61,43 @@ function breathee(){
 
 //array of objects, used in "for" option but figure out some way to reset the loop, as the infinity loop is not possible
 const timings = [
-    { funct: inhaleFunction, delay: 4000 },
-  { funct: holdFunction, delay: 6000 },
-  { funct: exhaleFunction, delay: 10000 },
-  { funct: inhaleFunction, delay: 16000 },
-  { funct: holdFunction, delay: 20000 },
-  { funct: exhaleFunction, delay: 24000 },
-  { funct: justBreatheFunction, delay: 34000 },
-  { funct: inhaleFunction, delay: 40000 },
-  { funct: holdFunction, delay: 44000 },
-  { funct: exhaleFunction, delay: 48000 },
-  { funct: justBreatheFunction, delay: 54000 },
-  { funct: inhaleFunction, delay: 60000 },
-  { funct: holdFunction, delay: 66000 },
-  { funct: exhaleFunction, delay: 70000 },
-  { funct: justBreatheFunction, delay: 76000 },
+    { funct: inhaleFunction, delay: 10000 },
+  { funct: holdFunction, delay: 14000 },
+  { funct: exhaleFunction, delay: 18000 },
+  { funct: holdFunction, delay: 24000 },
+  { funct: inhaleFunction, delay: 28000 },
+  { funct: holdFunction, delay: 32000 },
+  { funct: exhaleFunction, delay: 36000 },
+  { funct: justBreatheFunction, delay: 40000 },
+  { funct: inhaleFunction, delay: 50000 },
+  { funct: holdFunction, delay: 54000 },
+  { funct: exhaleFunction, delay: 58000 },
+  { funct: holdFunction, delay: 64000 },
+  { funct: inhaleFunction, delay: 68000 },
+  { funct: holdFunction, delay: 72000 },
+  { funct: exhaleFunction, delay: 76000 },
+  { funct: justBreatheFunction, delay: 80000 },
   { funct: inhaleFunction, delay: 90000 },
-  { funct: holdFunction, delay: 94000 },
-  { funct: exhaleFunction, delay: 100000 },
-  { funct: justBreatheFunction, delay: 110000 },
+{ funct: holdFunction, delay: 94000 },
+{ funct: exhaleFunction, delay: 98000 },
+{ funct: holdFunction, delay: 104000 },
+{ funct: inhaleFunction, delay: 108000 },
+{ funct: holdFunction, delay: 112000 },
+{ funct: exhaleFunction, delay: 116000 },
+{ funct: justBreatheFunction, delay: 120000 },
+{ funct: inhaleFunction, delay: 130000 },
+{ funct: holdFunction, delay: 134000 },
+{ funct: exhaleFunction, delay: 138000 },
+{ funct: holdFunction, delay: 144000 },
+{ funct: inhaleFunction, delay: 148000 },
+{ funct: holdFunction, delay: 152000 },
+{ funct: exhaleFunction, delay: 156000 },
+{ funct: justBreatheFunction, delay: 160000 },
+
 ];
 //adjust the timing
 //for the time being, little ugly but works for now
+//after some time, make screen giving help tips and reseting back to home screen
 
 for (const timing of timings) {
         setTimeout(timing.funct, timing.delay);
