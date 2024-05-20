67eedaa1c42b0b1c1ddf28568bc2bf43069ba71f
k <66728641+kingazm@users.noreply.github.com>
2024-05-20 17:36:55 +0200

    Update codeBreathee.js

--- codeBreathee.js
+++ codeBreathee.js
@@ -38,74 +38,39 @@ function draw() {
   window.requestAnimationFrame(draw);
 }
 
-//TEXT SECTION
+//BREATHING ASSISTANCE - TEXT DISPLAY SECTION
 
-init();
+function displayInstruction(instruction, timeout, countdown) {
+	setTimeout(function() {document.getElementById("instruction").innerHTML = instruction;}, currentTime);
+	currentTime += timeout;
 
-function inhaleFunction(){
-    document.getElementById("instruction").innerHTML = "inhale";
-}
-function exhaleFunction(){
-    document.getElementById("instruction").innerHTML = "exhale";
-}
-function holdFunction(){
-    document.getElementById("instruction").innerHTML = "hold";
-}
-function justBreatheFunction(){
-    document.getElementById("instruction").innerHTML = "try to take a few regular breaths";
-}
-function breathee(){
-    setTimeout(inhaleFunction(), 20000);
-    setTimeout(holdFunction(), 8000);
+	for (let i = countdown; i > 0; i--) {
+		setTimeout(function() {document.getElementById("instruction").innerHTML = i;}, currentTime);
+		currentTime += 1000;
+	}
 }
 
 function back(){
     window.location.assign("homePage.htm");
 }
 
-//array of objects, used in "for" option but figure out some way to reset the loop, as the infinity loop is not possible
-const timings = [
-    { funct: inhaleFunction, delay: 10000 },
-  { funct: holdFunction, delay: 14000 },
-  { funct: exhaleFunction, delay: 18000 },
-  { funct: holdFunction, delay: 24000 },
-  { funct: inhaleFunction, delay: 28000 },
-  { funct: holdFunction, delay: 32000 },
-  { funct: exhaleFunction, delay: 36000 },
-  { funct: justBreatheFunction, delay: 40000 },
-  { funct: inhaleFunction, delay: 50000 },
-  { funct: holdFunction, delay: 54000 },
-  { funct: exhaleFunction, delay: 58000 },
-  { funct: holdFunction, delay: 64000 },
-  { funct: inhaleFunction, delay: 68000 },
-  { funct: holdFunction, delay: 72000 },
-  { funct: exhaleFunction, delay: 76000 },
-  { funct: justBreatheFunction, delay: 80000 },
-  { funct: inhaleFunction, delay: 90000 },
-{ funct: holdFunction, delay: 94000 },
-{ funct: exhaleFunction, delay: 98000 },
-{ funct: holdFunction, delay: 104000 },
-{ funct: inhaleFunction, delay: 108000 },
-{ funct: holdFunction, delay: 112000 },
-{ funct: exhaleFunction, delay: 116000 },
-{ funct: justBreatheFunction, delay: 120000 },
-{ funct: inhaleFunction, delay: 130000 },
-{ funct: holdFunction, delay: 134000 },
-{ funct: exhaleFunction, delay: 138000 },
-{ funct: holdFunction, delay: 144000 },
-{ funct: inhaleFunction, delay: 148000 },
-{ funct: holdFunction, delay: 152000 },
-{ funct: exhaleFunction, delay: 156000 },
-{ funct: justBreatheFunction, delay: 160000 },
-{ funct: back, delay: 161000 },
+init();
+
+let currentTime = 2000;
+
+//breathing cycle
+for (let k = 0; k < 3; k++) {
+	for (let j = 0; j < 3; j++) {
+		displayInstruction("inhale", 4000, 0);
+		displayInstruction("hold", 1000, 5);
+		displayInstruction("exhale", 4000, 0);
+		displayInstruction("hold", 1000, 5);
+
+	}
+
+	displayInstruction("try taking a few regular breaths", 6000, 0);
+}
+
+setTimeout(function() { window.location.assign("homePage.htm");}, currentTime);
 
 
-];
-//adjust the timing
-//for the time being, little ugly but works for now
-//after some time, make screen giving help tips and reseting back to home screen
-
-
-for (const timing of timings) {
-        setTimeout(timing.funct, timing.delay);
-    }
